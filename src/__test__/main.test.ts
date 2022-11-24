f0878714dbf660821e41a054db4312170e3c0394
Angelica Reuterswärd <angelica@MacBook-Pro.local>
2022-11-24 16:34:11 +0100

    Dagens arbete m övningar - färdig m tester för functions.ts, precis börjat m main.ts

---
+++ src/__test__/main.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@jest/globals"; //kan även lägga in describe här, om använder i test.
+
+test("should x", () => {
+  //test för att testa funktion
+  //arrange
+  //act
+  //assert
+  //   expect().toBe();
+  //   expect().toBe();
+});
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,36 +4,41 @@ import { Todo } from "./models/Todo"; //import av klassen Todo.
 let todos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
 //listan av todos hämtas från localStorage.
 
-document.getElementById("clearTodos")?.addEventListener("click", () => {
-  clearTodos(todos);
-}); //hämtar knappen för att rensa listan.
-//FRÅGA: VARFÖR FRÅGETECKNET?
+export function init() {
+  document.getElementById("clearTodos")?.addEventListener("click", () => {
+    clearTodos(todos);
+  }); //hämtar knappen för att rensa listan och lyssnar efter klicket, om klick -> anropar funktion clearTodos.
 
-(document.getElementById("newTodoForm") as HTMLFormElement)?.addEventListener(
-  // FRÅGA: VARFÖR FRÅGETECKNET?
-  //det räcker med att lyssna på fomuläret, det är buttonklicket som "vandrar uppåt" och triggar vår submit som vi lyssnar på på vår form (det sker precis efter klicket).
-  "submit",
-  (e: SubmitEvent) => {
-    e.preventDefault(); //prevent default
-    /* normalt vid skicka formulär - skickar tillbaka till vår sida o allt töms. NU vill vi själva bestämma vad som ska hända istället! 
-    Vi vill preventDefault! Skriver därför in även e.preventDefault(); */
+  (document.getElementById("newTodoForm") as HTMLFormElement)?.addEventListener(
+    "submit",
+    //lyssnar på formuläret, lyssnar på klick = vid click->triggas formuläret/submit.
+    //(det räcker med att lyssna på fomuläret, det är buttonklicket som "vandrar uppåt" och triggar vår submit som vi lyssnar på på vår form (det sker precis efter klicket)).
+    //vid klick/submit ->anropas nedan funktion.
+    (e: SubmitEvent) => {
+      e.preventDefault(); //prevent default (förhindrar att formuläret töms).
+      /* normalt vid skicka formulär - skickar tillbaka till vår sida o allt töms. NU vill vi själva bestämma vad som ska hända istället! 
+      Vi vill preventDefault! Skriver därför in även e.preventDefault(); */
 
-    /* När vi anropar den anonyma funktionen av addEventListener får vi med annan info vi inte har pratat om ännu. 
-    Vi brukar kalla det för e eller event, denna parameter är ett objekt som är själva händelsen som precis inträffade. 
-    Objektet e har funktionalitet kopplat till sig, iom bygger på en klass (iom det är ett objekt i typescript). 
-    Vilken klass? Vi måste skriva datatypen. 
-    Detta är en inbyggd parameter som baseras på den händelse vi lyssnar efter. 
-    Just nu lyssnar vi på en submithändelse. Detta är i detta fall därför ngt som heter ett submitEvent!
-    Vi behöver skriva inom () på anonyma funktionen efter addEventListener (e: SubmitEvent) */
+      /* När vi anropar den anonyma funktionen av addEventListener får vi med annan info vi inte har pratat om ännu. 
+      Vi brukar kalla det för e eller event, denna parameter är ett objekt som är själva händelsen som precis inträffade. 
+      Objektet e har funktionalitet kopplat till sig, iom bygger på en klass (iom det är ett objekt i typescript). 
+      Vilken klass? Vi måste skriva datatypen. 
+      Detta är en inbyggd parameter som baseras på den händelse vi lyssnar efter. 
+      Just nu lyssnar vi på en submithändelse. Detta är i detta fall därför ngt som heter ett submitEvent!
+      Vi behöver skriva inom () på anonyma funktionen efter addEventListener (e: SubmitEvent) */
 
-    let todoText: string = (
-      document.getElementById("newTodoText") as HTMLInputElement
-    ).value; //hämtar vår inputtag och skapar en variabel av dess värde.
-    console.log("Todos when creating", todos);
+      let todoText: string = (
+        document.getElementById("newTodoText") as HTMLInputElement
+      ).value; //hämtar vår inputtag och skapar en variabel av dess värde.
+      console.log("Todos when creating", todos);
 
-    createNewTodo(todoText, todos); //anropar funktionen för att skapa ny todo samt skickar med inputvärdet todoText samt listan todos [].
-  }
-);
+      createNewTodo(todoText, todos); //anropar funktionen för att skapa ny todo samt skickar med inputvärdet todoText samt listan todos [].
+    }
+  );
+}
+window.onload = function () {
+  init();
+};
 
 function createNewTodo(todoText: string, todos: Todo[]) {
   let result = addTodo(todoText, todos); //ny variabel result fångar in resultatet av anrop av funktion addTodo - till vilken vi skickar med inputvärdet todoText samt listan todos []).
